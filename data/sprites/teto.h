6bad64e0b36fd26ec38e0c1a2dad49eb9903f476
Satchi <thiagoabreu429@gmail.com>
2025-06-20 13:19:04 -0300

    added teto sprite data

---
+++ data/sprites/teto.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(teto)
+
+//======================================================================
+//
+//	teto, 64x64@4, 
+//	+ palette 16 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 32 + 2048 = 2080
+//
+//	Time-stamp: 2025-06-20, 13:13:09
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TETO_H
+#define GRIT_TETO_H
+
+#define tetoTilesLen 2048
+extern const unsigned short tetoTiles[1024];
+
+#define tetoPalLen 32
+extern const unsigned short tetoPal[16];
+
+#endif // GRIT_TETO_H
+
+//}}BLOCK(teto)
