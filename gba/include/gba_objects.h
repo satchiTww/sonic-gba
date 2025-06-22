c94b207157c2d536dc318cb33eac40d81ec7d4ed
Satchi <thiagoabreu429@gmail.com>
2025-06-22 00:42:07 -0300

    changes in the obj structure and creation of gba_objects.c

--- gba/include/gba_objects.h
+++ gba/include/gba_objects.h
@@ -3,12 +3,29 @@
 
 #include "gba_video.h"
 
-//credits to Jasper “cearn” Vijn
+#define OAM_MAX_ENTRIES 128
 
 typedef struct {
-    u16 attr0;
-    u16 attr1;
-    u16 attr2;
+    //attribute 0
+    u32 yPos:8;
+    u32 affineMode:2;
+    u32 objMode:2;
+    u32 mosaic:1;
+    u32 colorMode:1;
+    u32 shape:2;
+
+    //attribute 1
+    u32 xPos:9;
+    u32 affineIndex:3; //lower 3 bits
+    u32 hFlip:1;
+    u32 vFlip:1;
+    u32 size:2;
+
+    //attribute 2
+    u16 tileID:10;
+    u16 bgPriority:2;
+    u16 paletteNum:4;
+
     s16 fill;
 } ALIGN4 OBJ_ATTR;
 
@@ -25,18 +42,33 @@ typedef struct {
 
 #define OAM_MEM ((OBJ_ATTR*)OAM)
 
-INLINE OBJ_ATTR *obj_set_attr(OBJ_ATTR *obj, u16 attr0, u16 attr1, u16 attr2)
-{
-    obj->attr0 = attr0;
-    obj->attr1 = attr1;
-    obj->attr2 = attr2;
-    return obj;
-}
+extern OBJ_ATTR obj_buffer[OAM_MAX_ENTRIES];
+
+#define OBJ_AFFINE        1
+#define OBJ_HIDE          2
+#define OBJ_AFFINE_DOUBLE 3
+
+#define OBJ_ALPHA_BLEND   1
+#define OBJ_WINDOW        2
+
+#define OBJ_4BPP          0
+#define OBJ_8BPP          1
+
+#define OBJ_SHAPE_SQUARE  0
+#define OBJ_SHAPE_WIDE    1
+#define OBJ_SHAPE_TALL    2
+#define OBJ_SIZE_0        0
+#define OBJ_SIZE_1        1
+#define OBJ_SIZE_2        2
+#define OBJ_SIZE_3        3
 
 INLINE void obj_set_pos(OBJ_ATTR *obj, int x, int y)
 {
-    obj->attr0 &= 0xFF00; obj->attr0 |= ATTR0_YPOS(y);
-    obj->attr1 &= 0xFE00; obj->attr1 |= ATTR1_XPOS(x);
+    //obj->attr0 &= 0xFF00; obj->attr0 |= ATTR0_YPOS(y);
+    //obj->attr1 &= 0xFE00; obj->attr1 |= ATTR1_XPOS(x);
+
+    obj->xPos = x;
+    obj->yPos = y;
 }
 
 #endif
---
+++ gba/src/gba_objects.c
@@ -0,0 +1,3 @@
+#include "gba_objects.h"
+
+OBJ_ATTR obj_buffer[OAM_MAX_ENTRIES];
