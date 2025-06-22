fce55240e5b414a96f038bf05cc49d6501562e3c
Satchi <thiagoabreu429@gmail.com>
2025-06-22 00:44:16 -0300

    put attributes for each object teto sprite is using

--- src/sprite_teto.c
+++ src/sprite_teto.c
@@ -1,8 +1,21 @@
 #include "sprite.h"
 #include "data/sprites/teto.h"
 
+static OBJ_ATTR obj_sprTeto[2]=
+{
+    [0].shape = OBJ_SHAPE_SQUARE,
+    [0].size = OBJ_SIZE_2,
+
+    [1].yPos = 32,
+    [1].shape = OBJ_SHAPE_WIDE,
+    [1].size = OBJ_SIZE_2,
+    [1].tileID = 16,
+};
+
 Sprite sprTeto=
 {
+    .width = 4,
+    .height = 6,
     .palette=
     {
         .data = tetoPal,
@@ -15,5 +28,7 @@ Sprite sprTeto=
         .lenght = tetoTilesLen,
         .char_block = OAM_CHARBLOCK,
         .char_block_index = 0
-    }
+    },
+    .objCount = 2,
+    .obj = obj_sprTeto
 };
