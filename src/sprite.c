125a8373d10da2fd02f2466a77db3e3252cb30ee
Satchi <thiagoabreu429@gmail.com>
2025-06-22 00:44:32 -0300

    loadm oam to oam buffer

--- src/sprite.c
+++ src/sprite.c
@@ -1,6 +1,6 @@
 #include "sprite.h"
 
-void load_sprite(Sprite *sprite)
+void load_sprite(Sprite *sprite, u8 xPos, u8 yPos, int objPriority, int bgPriority)
 {
     load_palette(
         sprite->palette.data,
@@ -12,5 +12,9 @@ void load_sprite(Sprite *sprite)
         sprite->tileset.char_block,
         sprite->tileset.char_block_index
     );
-    //TODO: load OAM
+    for (int i = 0; i < sprite->objCount; i++) {
+        obj_set_pos(&sprite->obj[i], sprite->obj[i].xPos + xPos, sprite->obj[i].yPos + yPos);
+        sprite->obj[i].bgPriority = bgPriority;
+        obj_buffer[objPriority + i] = sprite->obj[i];
+    }
 }
