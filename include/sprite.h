bd8f3c39f1d130f68bd804b35d7eb1e6365206f5
Satchi <thiagoabreu429@gmail.com>
2025-06-22 00:43:29 -0300

    changed sprite struct and load function

--- include/sprite.h
+++ include/sprite.h
@@ -6,10 +6,18 @@
 #include "gba_objects.h"
 
 typedef struct {
+    u8 xPos; //in px
+    u8 yPos; //in px
+    int width; //in tiles
+    int height; //in tiles
+    int objPriority;
+    int bgPriority;
     const Palette palette;
     const Tileset tileset;
+    int objCount;
+    OBJ_ATTR *obj;
 } Sprite;
 
-void load_sprite(Sprite *sprite);
+void load_sprite(Sprite *sprite, u8 xPos, u8 yPos, int objPriority, int bgPriority);
 
 #endif
