60095f9d9a9c085bc3705b1893d506a789a9ea35
Satchi <thiagoabreu429@gmail.com>
2025-06-22 00:41:02 -0300

    changed teto dimensions

--- data/sprites/teto.h
+++ data/sprites/teto.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	teto, 64x64@4, 
+//	teto, 32x48@4, 
 //	+ palette 16 entries, not compressed
-//	+ 64 tiles not compressed
-//	Total size: 32 + 2048 = 2080
+//	+ 24 tiles not compressed
+//	Total size: 32 + 768 = 800
 //
-//	Time-stamp: 2025-06-20, 13:13:09
+//	Time-stamp: 2025-06-21, 18:19:40
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_TETO_H
 #define GRIT_TETO_H
 
-#define tetoTilesLen 2048
-extern const unsigned short tetoTiles[1024];
+#define tetoTilesLen 768
+extern const unsigned short tetoTiles[384];
 
 #define tetoPalLen 32
 extern const unsigned short tetoPal[16];
