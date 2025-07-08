87d71d7817e88b080f8bc22dee2bf6a32c4dce27
Satchi <thiagoabreu429@gmail.com>
2025-07-08 17:22:00 -0300

    changed macros to inlines and moved fixed8 from gba typedefs to here

--- include/math_func.h
+++ include/math_func.h
@@ -1,9 +1,23 @@
 #ifndef MATH_FUNC_H
 #define MATH_FUNC_H
 
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define ABS(a) ((a) < 0 ? -(a) : (a))
-#define SIGN(a) ((a) > 0 ? 1 : ((a) < 0 ? -1 : 0))
+typedef s32 fixed8;
+
+#define FIXED8(integer, fraction) (((integer) << (8)) | ((fraction) & (0xFF)))
+
+INLINE s32 mf_min(s32 a, s32 b)
+{    return a < b ? a : b;    }
+
+INLINE s32 mf_max(s32 a, s32 b)
+{    return a > b ? a : b;    }
+
+INLINE s32 mf_abs(s32 a)
+{    return a < 0 ? -a : a;    }
+
+INLINE s32 mf_sign(s32 a)
+{    return a > 0 ? 1 : (a < 0 ? -1 : 0);    }
+
+INLINE s32 fixed8_to_int(fixed8 x)
+{    return (x >> 8);    }
 
 #endif
