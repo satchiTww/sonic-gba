8413949fdc6ac0228bb0d1c13cbd99c6b103a75c
Satchi <thiagoabreu429@gmail.com>
2025-07-12 23:45:28 -0300

    added collision data

---
+++ data/bin/collisionMap/TZ_CollisionMap.h
@@ -0,0 +1,7 @@
+#ifndef TZ_COLLISIONMAP_H
+#define TZ_COLLISIONMAP_H
+
+#define TZ_CollisionMapLength 882
+extern const unsigned short TZ_CollisionMap[882];
+
+#endif
---
+++ data/bin/solidTile/TZ_CollisionData.h
@@ -0,0 +1,10 @@
+#ifndef TZ_COLLISIONDATA_H
+#define TZ_COLLISIONDATA_H
+
+#define TZ_CollisionDataNumOfTiles 16
+
+extern const unsigned char TZ_CollisionDataHeight[256];
+
+extern const unsigned char TZ_CollisionDataAngle[16];
+
+#endif
